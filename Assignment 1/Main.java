23b8f1616abe71b5c0eb939126ff616bb91c07bb
Rachele Puri <67067674+helloimrachael@users.noreply.github.com>
2020-09-26 18:40:04 +0200

    Add files via upload

---
+++ Assignment 1/Main.java
@@ -0,0 +1,26 @@
+// Puri Rachele 547938
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        if(args.length < 2)
+            System.out.println("Usage: java Main arg1 arg2");
+        String accuracy = args[0];
+        String timeMax = args[1]; //tempo massimo di attesa
+        double acc = Double.parseDouble(accuracy);
+        Long tMax = Long.parseLong(timeMax);
+        piGreek p = new piGreek(acc);
+        Thread t = new Thread(p);
+        t.start();
+        System.out.println(t.getName() + " is active");
+        System.out.println("Start PI Greek calculation");
+        try{
+            t.join(tMax);
+            t.interrupt();
+            if(t.isInterrupted())
+                System.out.println(t.getName() + " interrupted");
+            System.out.println(Thread.currentThread().getName() + " is awake");
+        } catch (InterruptedException e){
+            System.out.println("Thread interrupted");
+        }  
+    }
+}
